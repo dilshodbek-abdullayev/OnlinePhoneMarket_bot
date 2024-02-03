fd90867419f997ee5dced352a8d2345602569edf
Dilshodbek <abdullayevdilshodbek1998@gmail.com>
2024-02-03 13:47:03 +0500

    request contact

--- OnlinePhoneMarket_bot/Program.cs
+++ OnlinePhoneMarket_bot/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("asd");
+﻿using _25_lesson_TelegramBot_Basic;
+
+try
+{
+    TelegramBot bot = new TelegramBot();
+    await bot.MainFunction();
+}
+catch (NullReferenceException)
+{
+    throw new Exception("Vay vay vay");
+}
+catch (Exception)
+{
+    throw new Exception("Nima bo'ldi");
+}
+
+//MyConvert convert = new MyConvert();
+//try
+//{
+//    convert.ReplayMessage().Wait();
+//}
+//catch (Exception)
+//{
+//    throw new Exception("Vay xatolik otdi.");
+//}
