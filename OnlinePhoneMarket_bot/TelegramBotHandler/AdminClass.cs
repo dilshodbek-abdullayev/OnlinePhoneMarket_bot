4b9962a33f9af9160ce5a4c4230106a51bf7b309
Dilshodbek <abdullayevdilshodbek1998@gmail.com>
2024-02-05 07:58:16 +0500

    chala

--- OnlinePhoneMarket_bot/TelegramBotHandler/AdminClass.cs
+++ OnlinePhoneMarket_bot/TelegramBotHandler/AdminClass.cs
@@ -1,14 +1,82 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
+﻿
 using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
-using Telegram.Bot;
-using Telegram.Bot.Types;
-using Telegram.Bot.Types.ReplyMarkups;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 namespace OnlinePhoneMarket_bot.TelegramBotHandler
 {
-   
+    public class Admin
+    {
+        public long chatId { get; set; }
+
+        public static string path = @"C:\Users\ACER\Desktop\GitHub\TelegramBot\OnlinePhoneMarket\OnlinePhoneMarket_bot\OnlinePhoneMarket_bot\Admins.json";
+
+        public static void Create(Admin admiin)
+        {
+            List<Admin> admins = DeserializeSerialize<Admin>.GetAll(path);
+            if (admins.Any(c => c.chatId == admiin.chatId))
+            {
+                return;
+            }
+            admins.Add(admiin);
+            DeserializeSerialize<Admin>.Save(admins, path);
+        }
+
+        public static string Read()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            List<Admin> admins = DeserializeSerialize<Admin>.GetAll(path);
+            foreach (Admin admin in admins)
+            {
+                stringBuilder.Append($"{admin.chatId}\n");
+            }
+            return stringBuilder.ToString();
+        }
+        public static bool isAdmin(long chatId)
+        {
+            List<Admin> admins = DeserializeSerialize<Admin>.GetAll(path);
+            if (admins.Any(c => c.chatId == chatId))
+            {
+                return true;
+            }
+            return false;
+        }
+        public static void Delete(long chatId)
+        {
+            try
+            {
+                List<Admin> admins = DeserializeSerialize<Admin>.GetAll(path);
+                var catToRemove = admins.Find(ct => ct.chatId == chatId);
+
+                if (catToRemove != null)
+                {
+                    admins.Remove(catToRemove);
+                    DeserializeSerialize<Admin>.Save(admins, path);
+                    DeserializeSerialize<Admin>.Save(admins, path);
+                }
+            }
+            catch { }
+        }
+    }
+    public static class DeserializeSerialize<T>
+    {
+        public static List<T> GetAll(string path)
+        {
+
+            if (System.IO.File.Exists(path))
+            {
+                string json = System.IO.File.ReadAllText(path);
+                return JsonSerializer.Deserialize<List<T>>(json) ?? new List<T>();
+            }
+            else
+            {
+                return new List<T>();
+            }
+        }
+        public static void Save(List<T> books, string path)
+        {
+            string json = JsonSerializer.Serialize(books);
+            System.IO.File.WriteAllText(path, json);
+        }
+    }
 }
---
+++ OnlinePhoneMarket_bot/TelegramBotHandler/Phones.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace OnlinePhoneMarket_bot.TelegramBotHandler
+{
+    public class Phones
+    {
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public long price { get; set; }
+
+        public static string path = @"C:\Users\ACER\Desktop\GitHub\TelegramBot\OnlinePhoneMarket\OnlinePhoneMarket_bot\OnlinePhoneMarket_bot\Phone.json";
+
+        public static void Create(Phones bk)
+        {
+            List<Phones> Phones = DeserializeSerialize<Phones>.GetAll(path);
+            if (Phones.Any(c => c.Model == bk.Model))
+            {
+                return;
+            }
+            Phones.Add(bk);
+            DeserializeSerialize<Phones>.Save(Phones, path);
+        }
+        public static string Read()
+        {
+            StringBuilder builder = new StringBuilder();
+            List<Phones> Phones = DeserializeSerialize<Phones>.GetAll(path);
+            foreach (Phones c in Phones)
+            {
+                builder.Append($"Phones Model:{c.Model}\n" +
+                    $"Phones Brand: {c.Brand}\n" +
+                    $"Phones price: {c.price}\n" + "\n---------------------" + "\n");
+            }
+            return builder.ToString();
+        }
+
+        public static string GetRead(Phones c)
+        {
+            return ($"Phone Model:{c.Model}\n" +
+                     $"Phone Brand: {c.Brand}\n" +
+                     $"Phone price: {c.price}\n" + "\n----------------------" + "\n");
+
+        }
+
+        public static void Update(string last_Model, string new_price, string new_Model, string new_Brand)
+        {
+            try
+            {
+                List<Phones> Phones = DeserializeSerialize<Phones>.GetAll(path);
+                if (Phones != null)
+                {
+                    int index = Phones.FindIndex(Model => Model.Model == last_Model);
+                    if (index != -1)
+                    {
+                        Phones[index].Model = new_Model;
+                        Phones[index].Brand = new_Brand;
+                        Phones[index].price = Convert.ToUInt16(new_price);
+                       
+                        DeserializeSerialize<Phones>.Save(Phones, path);
+                    }
+                }
+            }
+            catch { }
+        }
+
+        public static void Delete(string del_Model)
+        {
+            try
+            {
+                List<Phones> Phones = DeserializeSerialize<Phones>.GetAll(path);
+                var catchToRemove = Phones.Find(ct => ct.Model == del_Model);
+
+                if (catchToRemove != null)
+                {
+                    Phones.Remove(catchToRemove);
+                    DeserializeSerialize<Phones>.Save(Phones, path);
+                }
+            }
+            catch { }
+        }
+    }
+}
